980543cc78a4cde3cfba19fba7fae2f243d214be
BT616 <110855674+BT616@users.noreply.github.com>
2022-09-08 21:20:30 -0700

    trying to get localstorage to work

--- style.js
+++ style.js
@@ -1,12 +1,38 @@
-
-var timeEl = $("#currentDay")
+var timeEl = $("#currentDay");
+var userInputEl = document.querySelector(".userInput");
+var save = document.querySelector(".saveBtn");
 
 
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeEl.text(rightNow);
     
-  }
+  };
+
+  
+function renderSave(){
+var userStorage = localStorage.getItem("userStorage");
+userInputEl.textContent = userStorage;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
 
 
 
