3f8e566e87b816eace27543e5aec691e372dc8d8
BT616 <110855674+BT616@users.noreply.github.com>
2022-09-08 21:53:52 -0700

    try and render local storage to page

--- style.js
+++ style.js
@@ -2,18 +2,35 @@ var timeEl = $("#currentDay");
 var userInputEl = document.querySelector(".userInput");
 var save = document.querySelector(".saveBtn");
 
-
+//this is the date and time. set interval at the bottom displays it and counts per second "1000"
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeEl.text(rightNow);
+}
+
+
+//save button
+$(".saveBtn").on("click",function(){
+var user = $(this).siblings(".userInput").val();
+var time = $(this).parent().attr("id");
+localStorage.setItem(time,user);
+})
+
+
+
+// colorblocking 
+
+
+
+
+
+
+
+
+
+
+
     
-  };
-
-  
-function renderSave(){
-var userStorage = localStorage.getItem("userStorage");
-userInputEl.textContent = userStorage;
-};
 
 
 
@@ -22,34 +39,4 @@ userInputEl.textContent = userStorage;
 
 
 
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  setInterval(displayTime, 1000);
-
-
+setInterval(displayTime, 1000);
